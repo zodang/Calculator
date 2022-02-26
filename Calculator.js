90c4b684e3b85d2ade8cc67d02eb81ef11a36815
zodang <1141ekdms@naver.com>
2022-02-26 12:02:56 +0900

    fix

--- Calculator.js
+++ Calculator.js
@@ -1,65 +1,83 @@
 result = document.getElementsByClassName("result")[0];
 
-n = '';
+n = 0;
 current_operator = '';
 
+//숫자 받기
 function getValue(num) {
-    for (var i = 0; i <= 9; i++){
-        if (num == i) {
-            n = n*10 + i;
+    for (var i = 0; i < result.innerHTML.length; i++){
+        //소수점 계산
+        if (result.innerHTML.includes('.')) {
+            for (var i = 0; i <= 9; i++){
+                if (num == i) {
+                    n = n + i;
+                }
+            }            
+        }
+        else {
+            for (var i = 0; i <= 9; i++){
+                if (num == i) {
+                    n = n * 10 + i;
+                }
+            }
         }
     }
-    console.log(n);
     result.innerHTML = n;
-    return n;
 }
 
+//소수점 붙이기
+function dot() {
+    n = n + '.';
+    result.innerHTML = n;
+}
 
+//연산자 받기
 function getOperator(operator) {
-    n1 = n;
-    n = '';
+    //첫번째 계산일 때
+    if (result.innerHTML == 0) {
+        n1 = n;     
+    } 
+    // 두 번째 계산부터는 이전 계산 값에 이어서 계산
+    else {
+        n1 = result.innerHTML;
+    }
+    n = 0;
     current_operator = operator;    
 }
 
+//계산하기
 function getResult() {
     if (current_operator == 'plus') {
-        result.innerHTML = n + n1
+        result.innerHTML = n1*1 + n*1;
+        console.log(n1)
+        console.log(n)
+        console.log(result.innerHTML)
+        
     }
     else if (current_operator == 'minus') {
-        result.innerHTML = n1 - n;
+        result.innerHTML = n1*1 - n*1;
     }
     else if (current_operator == 'multi') {
-        result.innerHTML = n * n1;
+        result.innerHTML = n1*1 * n*1;
     }
     else if (current_operator == 'division') {
-        result.innerHTML = n1 / n;
+        result.innerHTML = n1*1 / n*1;
     }
 }
 
+//AC버튼
+function reset() {
+    n1 = 0;
+    n = 0;
+    result.innerHTML = 0
+}
 
-// var value = 0;
-// var value0 = 0;
+//+/-버튼
+function convertSign() {
+    result.innerHTML = -result.innerHTML
+}
 
-// function click_num(click_id) {
-//     if (click_id == "reset") {
-//         value = 0;
-//         result.innerHTML = value;
-//     }
-//     if (click_id == "sign") {
-//         value = -value; 
-//         result.innerHTML = value;
-//     }
-
-//     if (click_id == "percent") {
-//         value = value * 0.01;
-//         result.innerHTML = value;
-//     }
-    
-//     for (var i = 0; i <= 9; i++) {
-//         if (click_id == i) {
-//             value = value*10 + i;
-//             result.innerHTML = value;
-//         }
-//     }
-//     value0 = value;
-// }
+//%버튼
+function percent() {
+    result.innerHTML = result.innerHTML * 0.01
+}
